bb2d67d1b18bf6f9ccbea7c19438a44756b414e2
chengkang <15172356898@163.com>
2018-11-18 20:36:50 +0800

    将java工程上传到git

---
+++ idea-git-tesst/src/com/itheima/TestGit.java
@@ -0,0 +1,17 @@
+package com.itheima;
+
+/**
+ * @ClassName TestGit
+ * @Author 程康
+ * @Version 1.0
+ */
+public class TestGit {
+
+    public static void main(String[] args) {
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+        System.out.println("helloWorld");
+    }
+}
